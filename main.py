c8444c5f7446ae8cc2648477da59d5a42f4724fb
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-07-12 12:38:43 -0300

    Colocando knapsack em classe.

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
-def knapsack(peso_max, pesos, valores, n):
-    table = [[0 for _ in range(peso_max + 1)] for _ in range(n + 1)]
-    
-    for i in range(n + 1):
-        for peso_atual in range(peso_max + 1):
-            if i == 0 or peso_atual == 0:
-                table[i][peso_atual] = 0
-            elif pesos[i - 1] <= peso_atual:
-                table[i][peso_atual] = max(valores[i - 1] + table[i - 1][peso_atual - pesos[i - 1]], table[i - 1][peso_atual])
-            else:
-                table[i][peso_atual] = table[i - 1][peso_atual]
-    
-    return table[n][peso_max]
 
-valores = []
-pesos = []
 
-peso_max = int(input("Digite o peso máximo da sua mochila: "))
-n = int(input("Digite o número de itens a serem processados: "))
+class Knapsack:
 
-for i in range(n):
-    print(i + 1)
-    valor_item = int(input("Digite o valor do item: "))
-    peso_item = int(input("Digite o peso do item: "))
-    valores.append(valor_item)
-    pesos.append(peso_item)
+    def __init__(self):
 
-print("Valor máximo obtido:", knapsack(peso_max, pesos, valores, n))
+        self.valores = []
+        self.pesos = []
+
+        self.peso_max = 0
+        self.quantiaItens = 0
+
+    def set(self):
+
+        self.peso_max = int(input("Digite o peso máximo da sua mochila: "))
+        self.quantiaItens = int(
+            input("Digite o número de itens a serem processados: "))
+
+        for i in range(self.quantiaItens):
+            print(i + 1)
+            valor_item = int(input("Digite o valor do item: "))
+            peso_item = int(input("Digite o peso do item: "))
+            self.valores.append(valor_item)
+            self.pesos.append(peso_item)
+
+    def knapsack(self):
+        table = [[0 for _ in range(self.peso_max + 1)]
+                 for _ in range(self.quantiaItens + 1)]
+
+        for i in range(self.quantiaItens + 1):
+            for peso_atual in range(self.peso_max + 1):
+                if i == 0 or peso_atual == 0:
+                    table[i][peso_atual] = 0
+                elif self.pesos[i - 1] <= peso_atual:
+                    table[i][peso_atual] = max(
+                        self.valores[i - 1] + table[i - 1][peso_atual - self.pesos[i - 1]], table[i - 1][peso_atual])
+                else:
+                    table[i][peso_atual] = table[i - 1][peso_atual]
+
+        return table[self.quantiaItens][self.peso_max]
+
+
+if (__name__ == "__main__"):
+
+    knap = Knapsack()
+    knap.set()
+    print(knap.knapsack())
