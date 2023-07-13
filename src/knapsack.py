aa6879aaff09515f8c587a9da9b45c68781a41f3
DaviPierre <zafirosafira@gmail.com>
2023-07-12 23:15:25 -0300

    Atualização do knapsack

--- src/knapsack.py
+++ src/knapsack.py
@@ -19,47 +19,34 @@ class Knapsack:
             self.pesos.append(peso_item)
 
     def knapsack(self):
-        self.table = [
-            [0 for _ in range(self.peso_max + 1)]
-            for _ in range(self.quantiaItens + 1)
-        ]
-
+        self.table = [[0 for _ in range(self.peso_max + 1)] for _ in range(self.quantiaItens + 1)]
         list_itens_escolhidos = []
 
-        for i in range(self.quantiaItens + 1):
-            itens_escolhidos = []
-            for peso_atual in range(self.peso_max + 1):
-                self.rastreiaItens(itens_escolhidos)
-
-                if i == 0 or peso_atual == 0:
-                    self.table[i][peso_atual] = 0
-                elif self.pesos[i - 1] <= peso_atual:
-                    self.table[i][peso_atual] = max(
-                        self.valores[i - 1] + self.table[i - 1][peso_atual - self.pesos[i - 1]], self.table[i - 1][peso_atual])
+        for i in range(1, self.quantiaItens + 1):
+            for j in range(1, self.peso_max + 1):
+                if self.pesos[i - 1] <= j:
+                    self.table[i][j] = max(self.valores[i - 1] + self.table[i - 1][j - self.pesos[i - 1]], self.table[i - 1][j])
                 else:
-                    self.table[i][peso_atual] = self.table[i - 1][peso_atual]
-            list_itens_escolhidos.append(self.filter(itens_escolhidos))
-            itens_escolhidos=[]
+                    self.table[i][j] = self.table[i - 1][j]
 
+        for i in range(1, self.quantiaItens + 1):
+            itens_escolhidos = self.rastreiaItens(i)
+            list_itens_escolhidos.append(itens_escolhidos)
 
         return self.table[self.quantiaItens][self.peso_max], list_itens_escolhidos
 
-
-    def filter(self,lista):
-        aux=[]
-        for item in lista:
-            if item not in aux:
-                aux.append(item)
-        return aux
-
-    def rastreiaItens(self, itens_escolhidos):
-        i = self.quantiaItens
-        peso_atual = self.peso_max
-        while i > 0 and peso_atual > 0:
-            if self.table[i][peso_atual] != self.table[i - 1][peso_atual]:
+    def rastreiaItens(self, linha):
+        itens_escolhidos = []
+        i = linha
+        j = self.peso_max
+        while i > 0 and j > 0:
+            if self.table[i][j] != self.table[i - 1][j]:
                 itens_escolhidos.append(i)
-                peso_atual -= self.pesos[i - 1]
+                j -= self.pesos[i - 1]
             i -= 1
+        itens_escolhidos.reverse()
+        return itens_escolhidos
+
 
     def get_table(self):
         return self.table
--- src/main.py
+++ src/main.py
@@ -32,12 +32,11 @@ if (__name__ == "__main__"):
     knapsack = Knapsack()
     knapsack.set()
     max_valor, itens_escolhidos = knapsack.knapsack()
-    table = knapsack.get_table()
 
-    # Imprimir a tabela
     for row in itens_escolhidos:
         print(row)
 
     App(itens_escolhidos)
 
-    # print(itens_escolhidos)
+
+
